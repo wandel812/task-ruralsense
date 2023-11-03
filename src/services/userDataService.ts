8107287c6ddc51f1278223ef19b7ded5995df509
Dmitriy Ivanov <ivanovdm812@gmail.com>
2023-11-03 09:55:43 +0400

    add userService

---
+++ src/services/userDataService.ts
@@ -0,0 +1,95 @@
+import { v4 as uuidv4 } from 'uuid';
+
+enum UserRole {
+    ADMIN = "ADMIN",
+    MANAGER = "MANAGER",
+}
+
+type User = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    isActivated: boolean;
+    role: UserRole;
+}
+
+
+const fetchUserData = () => {
+    new Promise(resolve => {
+        setTimeout(() => resolve(generateUserData()), 1000);
+    })
+}
+
+function generateUserData(): User[] {
+    return ([
+        {
+            id: uuidv4(),
+            firstName: "Alice",
+            lastName: "Smith",
+            isActivated: true,
+            role: UserRole.ADMIN
+        },
+        {
+            id: uuidv4(),
+            firstName: "Bob",
+            lastName: "Smith",
+            isActivated: false,
+            role: UserRole.MANAGER
+        },
+        {
+            id: uuidv4(),
+            firstName: "Nelly",
+            lastName: "Kit",
+            isActivated: true,
+            role: UserRole.ADMIN
+        },
+        {
+            id: uuidv4(),
+            firstName: "Aaa",
+            lastName: "Aaaaa",
+            isActivated: true,
+            role: UserRole.MANAGER
+        },
+        {
+            id: uuidv4(),
+            firstName: "Ccc",
+            lastName: "Ccccc",
+            isActivated: false,
+            role: UserRole.ADMIN
+        },
+        {
+            id: uuidv4(),
+            firstName: "Ddd",
+            lastName: "Ddddd",
+            isActivated: true,
+            role: UserRole.MANAGER
+        },
+        {
+            id: uuidv4(),
+            firstName: "Eee",
+            lastName: "Eeeee",
+            isActivated: false,
+            role: UserRole.ADMIN
+        },
+        {
+            id: uuidv4(),
+            firstName: "Fff",
+            lastName: "Fffff",
+            isActivated: false,
+            role: UserRole.MANAGER
+        },
+        {
+            id: uuidv4(),
+            firstName: "Ggg",
+            lastName: "Ggggg",
+            isActivated: true,
+            role: UserRole.ADMIN
+        },
+        
+
+    ])
+}
+
+export {UserRole, fetchUserData};
+
+
